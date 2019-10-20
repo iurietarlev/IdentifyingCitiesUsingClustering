7970c9ee69f74ffbb26da660ccf6741348ee919b
osk849 <iura.tarlev@gmail.com>
2019-10-20 12:33:19 +0100

    Added some comments

--- main.R
+++ main.R
@@ -17,54 +17,38 @@ getwd()
 
 
 
-#IMPORTING ALL THE NECESSARY FUNCTIONS CREATED IN ANOTHER FILE
+# --- IMPORTING ALL THE NECESSARY FUNCTIONS CREATED IN ANOTHER FILE --- #
 source("./mainFunctions.R")
 
-#rm(list=ls()) # to clean the environment
+# --- RUNNING SCRIPT --- #
 
 
-
-
-#----@@@@@@@@@@@----RUNNING SCRIPT ----@@@@@@@@@@@@@@@@@@@@
-
-
-#change seed_no for possiility of reproducing same experiment
+# change seed_no for possiility of reproducing same experiment
 seed_number <- 600
+
 
 region_analysed = "Bath and North East Somerset"
 df_selection <- region_selector(region_analysed)
-df_selection
 
-
-#____extract only parameter/number columns for PCA
-
-
+# pca transformed dataset
 pca_df <-pca_transform(df_selection, region_analysed, 6, full_name =FALSE)
-pca_df
 
+# t-SNE transformed dataset
 tsne_df <- tsne_transform(df_selection, region_analysed, perplexity = 10, max_iter = 10000, seed = 600,
                           full_name = FALSE)
-tsne_df
-#------------------------------------CLUSTERING ANALYSIS--------------------------
 
+# --- CLUSTERING ANALYSIS --- #
 
-#region_analysed = "TRIAL2"
-# -------------------Plotting of Silhouettes -------------------------------------
-# create output folder
+# --- Plotting of Silhouettes --- #
 direction <- sprintf("./%s_CLUST_OUTPUTS", region_analysed)
 dir.create(file.path(direction), showWarnings = FALSE) 
 
-
-
+# get the silhouette coefficients for both datasets for various clustering algos
 PCA_silhouettes <- silhouettes(pca_df, 2, 20)
-PCA_silhouettes
-
-
-
-
 TSNE_silhouettes <- silhouettes(tsne_df, 2, 20)
-TSNE_silhouettes
 
+
+# plot the silhouette coefficients against no of clusters and with clustering types
 plot_silhouettes(PCA_silhouettes, TSNE_silhouettes, "Set1", axis_text_size = 10,
                  legend_text_size = 10, title_size = 10, aspect_ratio = 1, "Number of Clusters", "Silhouette Coefficient", 
                  point_size = 0.65,
@@ -72,21 +56,19 @@ plot_silhouettes(PCA_silhouettes, TSNE_silhouettes, "Set1", axis_text_size = 10,
 
 
 
+# create  a directory for exporting of rds files with clustering info
 rds_direction <- sprintf("./%s_RDS_CLUST_DF_OUTPUTS", region_analysed)
-
 dir.create(file.path(rds_direction), showWarnings = FALSE) 
 
 
-
-
-#PLOTTING PCA SIL & CLUST PLOTS\
+# --- PLOTTING PCA SIL & CLUST PLOTS --- #
 # change geom_type to "text" if the last 4 digits of LA_CODE need to be retained
 clust_sil_plots(pca_df, 6, "PCA", direction, geom_type = "point", 
                 axis_text_size = 10, legend_text_size = 9, title_size = 10,
                 label_size = 5, point_size = 0.3, leg_size = 10, leg_width = 10, 
                 plot_width = 150, plot_height = 240)
 
-#PLOTTING tSNE SIL & CLUST PLOTS
+# --- PLOTTING tSNE SIL & CLUST PLOTS --- #
 # change geom_type to "text" if the last 4 digits of LA_CODE need to be retained
 clust_sil_plots(tsne_df, 6, "t-SNE", direction, geom_type = "point", 
                 axis_text_size = 10, legend_text_size = 9, title_size = 10,
@@ -104,7 +86,6 @@ pca_df_plot <-pca_transform(df_selection, region_analysed, 6, full_name =TRUE)
 tsne_df_plot <- tsne_transform(df_selection, region_analysed, perplexity = 10, max_iter = 10000, seed = 600,
                           full_name = TRUE)
 
-#save_df_to_rds <- function(df, nclust, dim_red_method, rds_dir)
 save_df_to_rds(tsne_df_plot,6, "tSNE", rds_direction)
 save_df_to_rds(pca_df_plot, 6, "PCA", rds_direction)
 
